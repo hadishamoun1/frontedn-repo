c56c703402546cc5602f4f66b26f9168273348f0
hadishamoun1 <shamounhadi2@gmail.com>
2024-07-07 21:55:59 +0300

    added python config fo yaml

--- python.py
+++ python.py
@@ -1,1 +1,9 @@
-print('hello world')
+import yaml
+
+with open('config.yaml', 'r') as stream:
+    config = yaml.safe_load(stream)
+
+if config['run_localhost']:
+    print("Running in localhost mode.")
+else:
+    print("Not running in localhost mode.")
