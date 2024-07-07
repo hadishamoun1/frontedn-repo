49db32fe7c61185449155281fcbe23963c1cc982
hadishamoun1 <shamounhadi2@gmail.com>
2024-07-08 00:10:53 +0300

    edited python file

--- python.py
+++ python.py
@@ -1,1 +1,11 @@
-print('hello all ')
+import yaml
+import os
+
+config_file = os.path.join(os.path.dirname(__file__), 'C:/Users/shamo/Desktop/projects/fullstack-repo/config.yaml')
+
+with open(config_file, 'r') as f:
+    config = yaml.safe_load(f)
+
+server_ip = config.get('ip_address')
+
+print(f"Connecting to server at IP address: {server_ip}")
